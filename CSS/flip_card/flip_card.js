e9e4303fa240d23379707d809f388cfeebe79b81
Nagarjun2210 <arjun79043@gmail.com>
2024-08-26 17:29:46 +0530

    flip card edited

--- CSS/flip_card/flip_card.js
+++ CSS/flip_card/flip_card.js
@@ -3,7 +3,13 @@ const front = document.getElementById('front');
 const back = document.getElementById('back');
 
 function flipCard() {
-    front.classList.toggle('front-rotate');
-    back.classList.toggle('back-rotate');
+    front.classList.add('front-rotate');
+    back.classList.add('back-rotate');
     console.log("card clicked");
 };
+
+function flipBack() {
+    front.classList.remove('front-rotate');
+    back.classList.remove('back-rotate');
+    console.log("card clicked");
+}
